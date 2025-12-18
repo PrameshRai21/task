646f6936ca92a6bb7bf468a954d8e6476444c8d8
PrameshRai21 <prameshchamling2@gmail.com>
2025-12-18 14:55:45 +0545

    feat: objective component feature

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,11 +1,18 @@
 import Dashboard from '@Views/Dashboard';
 import { IRoute } from './types';
+import About from '@Views/About';
 
 const appRoutes: IRoute[] = [
   {
     path: '/',
-    name: 'Dashboard ',
+    name: 'Dashboard',
     component: Dashboard,
+    authenticated: false,
+  },
+   {
+    path: '/about',
+    name: 'About',
+    component: About,
     authenticated: false,
   },
 ];
